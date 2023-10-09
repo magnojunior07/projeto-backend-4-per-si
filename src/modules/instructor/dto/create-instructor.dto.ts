88dfea903990d25dc694443b212ba407464593ca
magnojunior07 <magnojunior319@gmail.com>
2023-10-08 22:12:55 -0400

    refac: remove class validator from instructor dto in passowrd property

--- src/modules/instructor/dto/create-instructor.dto.ts
+++ src/modules/instructor/dto/create-instructor.dto.ts
@@ -6,8 +6,6 @@ export class CreateInstructorDto implements IInstructor {
     @IsString({ message: "name must be a string" })
     name: string;
 
-    @IsNotEmpty({ message: "password is required" })
-    @IsString({ message: "password must be a string" })
     createdAt?: Date;
     updatedAt?: Date;
 }
