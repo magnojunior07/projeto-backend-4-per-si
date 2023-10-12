a8265e5d781294950afa6b226170b4fa71215d50
magnojunior07 <magnojunior319@gmail.com>
2023-10-11 22:27:27 -0400

    refac: remove unsed import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UserModule } from "./modules/user/user.module";
-import DatabaseService from "./database/database.service";
 import { InstructorModule } from "./modules/instructor/instructor.module";
 import { CourseModule } from "./modules/course/course.module";
 import { SubscriptionModule } from "./modules/subscription/subscription.module";
